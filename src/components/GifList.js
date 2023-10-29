ddbb22fda88145eb01b9257f97f8daa4f40cb34e
pranavubarhande <pranavubarhande@gmail.com>
2023-10-29 08:08:02 +0530

    removed unncessary functions

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -3,29 +3,9 @@ import { FlatList, View } from 'react-native';
 import GifItem from './GifItem';
 import BigList from "react-native-big-list";
 const GifList = ({ gifs, onGifPress, onEndReached, itemsPerPage }) => {
-  const numColumns = 2;
-
-  const formatData = useCallback((data, numColumns) => {
-    const numberOfFullRows = Math.floor(data.length / numColumns);
-    let numberOfElementsInLastRow = data.length - numberOfFullRows * numColumns;
-
-    while (
-      numberOfElementsInLastRow !== numColumns &&
-      numberOfElementsInLastRow !== 0
-    ) {
-      data.push({ key: `blank-${numberOfElementsInLastRow}`, empty: true });
-      numberOfElementsInLastRow++;
-    }
-
-    return data;
-  }, []);
-
-  const formattedData = formatData(gifs, numColumns);
-
   
-
   const renderGifItem = ({ item }) => {
-    if (item.empty) {
+    if (item == null) {
       return <View style={{ flex: 1, margin: 4 }} />;
     }
     return <GifItem gif={item} onPress={() => onGifPress(item)} />;
